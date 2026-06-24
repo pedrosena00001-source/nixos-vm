76346f5af8ce84a636e08cfabfc47fd3c4948a84
pedro <pedro.sena00001@gmail.com>
2026-06-24 13:43:27 -0300

    alacritty arquivo separado

---
+++ alacritty.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+ 
+
+programs.alacritty = { 
+         enable = true;
+         settings = {
+           window = {
+               opacity = 0.50;
+               padding = {
+                   x = 10;
+                   y = 10;
+                };
+                decorations = "full";
+               };
+                                                               
+               font = {
+                    normal = {
+                           family = "JetBrainsMono Nerd Font";
+                            style = "Regular";
+                    };
+                    size = 12.0;
+                 };
+                 colors = { 
+                        primary = {
+                             background = "#1e1e2e";
+                             foreground = "#cdd6f4";
+                         };
+                       };
+                                                               
+                 cursor = {
+                    style = "Block";
+           };
+         };
+       };
