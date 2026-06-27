deeef0dbd29d46cbccda036f1c03646ce087fdba
pedro <pedro.sena00001@gmail.com>
2026-06-27 01:17:16 -0300

    arquivo separado do zsh

---
+++ zsh.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... } 
+
+programs.zsh = {
+          enable = true;
+          autosuggestions.enable = true;    
+          syntaxHighlighting.enable = true; 
+          enableCompletion = true;
+         ohMyZsh = {
+              enable = true;
+              theme = "cloud";
+              plugins = [
+                  "git"
+                  "sudo" 
+                  "history"
+                  "docker"
+            shellAliases = {
+                   ll = "ls -la";
+                   sv = "sudo vim";
+                   update = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixos-btw";
+                   v = "vim";
+                   garbage = "sudo nix-collect-garbage -d";
+                   cdd = "cd nixos-dotfiles";
+             };
+ 
+          ];
+       };
+    };
